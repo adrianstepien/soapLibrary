76de98ef16709553e5a87e903d5562d067561e4b
adrianstepien <adrianstepien2702@gmail.com>
2019-09-16 21:59:41 +0200

    add wsdl

--- src/main/java/com/stock/book/controller/BookEndpoint.java
+++ src/main/java/com/stock/book/controller/BookEndpoint.java
@@ -1,5 +1,9 @@
 package com.stock.book.controller;
 
+import com.adrianbooks.springsoap.gen.Book;
+import com.adrianbooks.springsoap.gen.GetBooksRequest;
+import com.adrianbooks.springsoap.gen.GetBooksResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -10,10 +14,21 @@ public class BookEndpoint {
 
     private static final String NAMESPACE_URI = "http://www.adrianBooks.com/springsoap/gen";
 
-    /*@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getBooksRequest")
+    @Autowired
+    public BookEndpoint() {
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getBooksRequest")
     @ResponsePayload
     public GetBooksResponse getBooks(@RequestPayload GetBooksRequest request) {
         GetBooksResponse response = new GetBooksResponse();
-        response.setBook
-    }*/
+        Book book = new Book();
+        book.setId(1);
+        book.setAuthors("J.R.R. Tolkien");
+        book.setDescription("Ksiazka o pierscieniu");
+        book.setTitle("Wladca pierscieni");
+        response.setBook(book);
+
+        return response;
+    }
 }
